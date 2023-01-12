ce11857ff6b22d3859161f2f30ea7ca653237198
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-01-12 17:33:40 +0000

    added bag app and template and javascript to keep the footer from rising but it has not worked

--- sister_resin_craft/settings.py
+++ sister_resin_craft/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
-    'products'
+    'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	let footerheight = $("#footer").height();
+	$("body").css({
+		"padding-bottom": footerheight,
+	});
+});
